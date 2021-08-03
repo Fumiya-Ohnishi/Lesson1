d7d88335f58fe5120d7dc507155c05224decb08c
oonisifumiya <soccer.14.2.25@gmail.com>
2021-08-03 17:50:45 +0900

    file追加

---
+++ saikoro.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="saikoro.css">
+    <script src="saikoro.js"></script>
+    <title>サイコロ</title>
+</head>
+
+<body>
+    <p><input type="button" id="btn" value="サイコロを振る" onclick="saikoro()"></p>
+    <div id="saikoro"></div>
+</body>
+
+</html>
